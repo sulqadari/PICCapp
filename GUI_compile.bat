135731f00c37b695704874a62f954baf4dcd8dc0
Telman <codejava@yandex.ru>
2020-06-25 00:07:51 +0300

    [GUI] - переехал на openJDK 14 + openFX 14.

--- GUI_compile.bat
+++ GUI_compile.bat
@@ -1,4 +1,10 @@
 echo off
+set JAVA_HOME=C:\openJDK\jdk-14
+set PATH=%JAVA_HOME%\bin;%PATH%
+echo Display Java Version
+java -version
+
 chcp 1251
-javac -sourcepath ./src/gui;./src -d ./bin/gui ./src/gui/TroikaAppMain.java
+rem javac -sourcepath ./src -d ./bin src/AudioConfigMain.java
+javac --module-path %PATH_TO_FX% --add-modules javafx.controls ./src/gui/TroikaAppMain.java -sourcepath ./src/gui;./src -d ./bin/gui
 pause
--- GUI_run.bat
+++ GUI_run.bat
@@ -1,3 +1,10 @@
 echo off
-java -cp ./bin/gui TroikaAppMain
+set JAVA_HOME=C:\openJDK\jdk-14
+set PATH=%JAVA_HOME%\bin;%PATH%
+echo Display Java Version
+java -version
+
+chcp 1251
+rem javac -sourcepath ./src -d ./bin src/AudioConfigMain.java
+java --module-path %PATH_TO_FX% --add-modules javafx.controls -cp ./bin/gui TroikaAppMain
 pause
